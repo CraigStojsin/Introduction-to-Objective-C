9553453e0020835653f057bb35faa5550e8ce625
CraigStojsin <stojsin@outlook.com>
2015-04-07 15:25:03 -0400

    Initial

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -10,8 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //comment section
+        // This is an inline comment
+        
+        /* This is a block comment.
+         It can span multiple lines. */
+        
+        //Variable Section
+        
+        double odometer = 9200.8;
+        int odometerAsInteger = (int)odometer;
+        
+        NSLog(@"You've driven %.1f miles", odometer);        // 9200.8
+        NSLog(@"You've driven %d miles", odometerAsInteger); // 9200
     }
     return 0;
 }
