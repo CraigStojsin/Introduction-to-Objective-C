be0c198227c12a6ccf3ce56ec16db0623e0cd36c
CraigStojsin <stojsin@outlook.com>
2015-04-07 22:06:17 -0400

    This is my first day at bitmaker

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -8,51 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-       //pointers section
-        
-        int year = 1967;          // Define a normal variable
-        int *pointer;             // Declare a pointer that points to an int
-        pointer = &year;          // Find the memory address of the variable
-        NSLog(@"%d", *pointer);   // Dereference the address to get its value
-        *pointer = 1990;          // Assign a new value to the memory address
-        NSLog(@"%d", year);       // Access the value via the variable
-        }
+int main (int argc, const char * argv[]) {
+    @autoreleasepool
+    {
     
-    char model[5] = {'H', 'o', 'n', 'd', 'a'};
-    char *modelPointer = &model[0];
-    for (int i=0; i<5; i++) {
-        NSLog(@"Value at memory address %p is %c",
-              modelPointer, *modelPointer);
-        modelPointer++;
+        NSLog(@"This is my first day at bitmaker labs");
+    
+    
     }
-    NSLog(@"The first letter is %c", *(modelPointer - 5));
-   //null pointer
-    int years = 1967;
-    int *pointer = &years;
-    NSLog(@"%d", *pointer);     // Do something with the value
-    pointer = NULL;             // Then invalidate it
-    //void pointer
-    int year1 = 1967;
-    void *genericPointer = &year1;
-    int *intPointer = (int *)genericPointer;
-    NSLog(@"%d", *intPointer);
-    
-    /*- (id)initWithBytes:(const void *)bytes
-length:(NSUInteger)length
-encoding:(NSStringEncoding)encoding*/
-    
-    NSString *moodel = @"Honda";
-    
-    NSString *anObject;    // An Objective-C object
-    anObject = NULL;       // This will work
-    anObject = nil;        // But this is preferred
-    int *aPointer;         // A plain old C pointer
-    aPointer = nil;        // Don't do this
-    aPointer = NULL;       // Do this instead
-    
-    
     
     
     
