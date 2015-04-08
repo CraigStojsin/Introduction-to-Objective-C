6417490b7092d669cbb12c72540fc9c1a8be4a46
CraigStojsin <stojsin@outlook.com>
2015-04-08 09:57:46 -0400

    Last question

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -6,17 +6,15 @@
 //  Copyright (c) 2015 Codes By Craig. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
-int main (int argc, const char *argv[]);
-(@autoreleasepool; {
-
-    
-    int sum = 0;
-    //COMPUTE RESULT
-    sum = 25 + 37 - 19
-    // DISPLAY RESULTS
-   Nslog(@"The answer is %i", sum);
-
-return 0;
+int main (int argc, const char *argv[])
+{
+    @autoreleasepool {
+        int answer, result;
+        answer = 100;
+        result = answer - 10;
+        NSLog (@"The result is %i\n", result + 5);// the result is 95
+    }
+    return 0; 
 }
-
