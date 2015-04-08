6feab9c4b2a723da7125d4fda427216c432366d1
CraigStojsin <stojsin@outlook.com>
2015-04-07 21:19:38 -0400

    Loop Statements

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -11,36 +11,75 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        //switch statements ( only work with integral types)
+       
+        int modelYear2 = 1990;
         
-        int modelYear = 1990;
+        //while loops
         
-        switch (modelYear) {
+        
+        
+        int  a = 0;
+        
+        while (a<5){
+            
+            if (a == 3){
                 
-            case 1987:
-                
-                NSLog(@"your car is from 1987.");
-                
-                break;
-                
-            case 1988:
-                
-                NSLog(@"your car is from 1988.");
-                
-            case 1989:
-                
-            case 1990:
-                
-                NSLog(@"your car is from 1989 or 1990");
+                NSLog(@"Aborting the while-loop");
                 
                 break;
                 
-            default:
+            }
+            
+            
+            
+            NSLog(@"Current year: %d",modelYear2 + a);
+            
+            a++;
+            
+            
+            
+        }
+        
+        
+        
+        //for loops
+        
+        for (int a=0; a<5; a++) {
+            
+            if (a == 3) {
                 
-                NSLog(@"i have no idea when your car was made");
+                NSLog(@"Skipping a for-loop iteration");
                 
-                break;
+                continue;
                 
+            }
+            
+            NSLog(@"Current year: %d", modelYear2 + a);
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        // For-in loops ("Fast-enumetation, specific to Objective-C")
+        
+        
+        
+        NSArray* models = @[@"Ford",@"Honda",@"Nissan",@"Porshe"];
+        
+        for (id model in models) {
+            
+            NSLog(@"%@",model);
+            
+            
+            
         }
         
         
