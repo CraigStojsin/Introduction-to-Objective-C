52e81dce71f547d8d82028263239536705b351af
CraigStojsin <stojsin@outlook.com>
2015-04-07 21:18:01 -0400

    Switch Statement

--- Assignment3/main.m
+++ Assignment3/main.m
@@ -11,25 +11,40 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        /*Operator description
-         
-         a == b  Equal to
-         
-         a != b  Not equal to
-         
-         a > b   Greater than
-         
-         a >= b  Greater than or equal to
-         
-         a < b   Less than
-         
-         a <= b  Less than or equal to
-         
-         !a  Logical negation
-         
-         a && b  Logical and
-         
-         a || b  Logical or */
+        //switch statements ( only work with integral types)
+        
+        int modelYear = 1990;
+        
+        switch (modelYear) {
+                
+            case 1987:
+                
+                NSLog(@"your car is from 1987.");
+                
+                break;
+                
+            case 1988:
+                
+                NSLog(@"your car is from 1988.");
+                
+            case 1989:
+                
+            case 1990:
+                
+                NSLog(@"your car is from 1989 or 1990");
+                
+                break;
+                
+            default:
+                
+                NSLog(@"i have no idea when your car was made");
+                
+                break;
+                
+        }
+        
+        
+        
         
 
         
